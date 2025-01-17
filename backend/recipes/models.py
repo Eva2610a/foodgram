ab3b429e98d0ff9c8ba33d5273dfa24677beb0bd
Ева Богунова <bogunova.eva@yandex.ru>
2025-01-18 00:16:33 +0500

    йоу

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils.crypto import get_random_string
 
-from api.const import MESSAGE, MES_MAX
+from ..api.const import MESSAGE, MES_MAX
 
 User = get_user_model()
 
